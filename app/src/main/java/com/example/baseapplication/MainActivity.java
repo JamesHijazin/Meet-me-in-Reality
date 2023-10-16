b04b3bad558eb19faca500516abcac9ee63cffd1
Tung Nguyen <68981093+s3871942@users.noreply.github.com>
2023-10-16 23:22:41 +1100

    initial_commit

---
+++ app/src/main/java/com/example/baseapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.baseapplication;
+
+import java.io.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottonnav);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        loadFragment(new DashboardFragment());
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        if (item.getItemId() == R.id.dashboard) {
+            fragment = new DashboardFragment();
+        } else if (item.getItemId() == R.id.users) {
+            fragment = new UsersFragment();
+        } else if (item.getItemId() == R.id.profile) {
+            fragment = new ProfileFragment();
+        }
+
+        if (fragment != null) {
+            loadFragment(fragment);
+        }
+
+        return true;
+    }
+
+    void loadFragment(Fragment fragment) {
+        //to attach fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.relativelayout, fragment).commit();
+    }
+}
