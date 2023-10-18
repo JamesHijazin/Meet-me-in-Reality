9210ba0c450352332a7ad389b6da682ba8b24b01
SenaT03 <81272285+SenaT03@users.noreply.github.com>
2023-10-19 08:14:52 +1100

    Added Map API and user interaction

--- app/src/main/java/com/example/meetmeinreality/MainActivity.java
+++ app/src/main/java/com/example/meetmeinreality/MainActivity.java
@@ -1,24 +1,40 @@
 package com.example.meetmeinreality;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng; // Add this import
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Marker;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     FirebaseAuth auth;
     Button button;
     TextView textView;
     FirebaseUser user;
+
+    private Marker marker;
+    double landmarkLatitude = 37.8102;
+    double landmarkLongitude = 144.9628;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -30,8 +46,7 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), Login.class);
             startActivity(intent);
             finish();
-        }
-        else {
+        } else {
             textView.setText(user.getEmail());
         }
 
@@ -45,5 +60,81 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap googleMap) {
+                    // Add markers and configure the map here
+                    LatLng landmarkLocation = new LatLng(landmarkLatitude, landmarkLongitude);
+                    MarkerOptions markerOptions = new MarkerOptions()
+                            .position(landmarkLocation)
+                            .title("Landmark Name")
+                            .draggable(true); // Make the marker draggable
+
+                    Marker marker = googleMap.addMarker(markerOptions);
+
+                    // Set an OnMarkerClickListener to handle marker interactions
+                    googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                        @Override
+                        public boolean onMarkerClick(Marker marker) {
+                            // Create a custom dialog to let the user interact with the marker
+                            showCustomMarkerDialog(marker);
+                            return true; // To consume the event and prevent default behavior
+                        }
+                    });
+
+                    // Set an OnMarkerDragListener to detect when the marker is dragged
+                    googleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+                        @Override
+                        public void onMarkerDragStart(Marker marker) {
+
+                        }
+
+                        @Override
+                        public void onMarkerDrag(Marker marker) {
+
+                        }
+
+                        @Override
+                        public void onMarkerDragEnd(Marker marker) {
+
+                        }
+                    });
+                }
+            });
+        }
+
     }
+
+    private void showCustomMarkerDialog(final Marker marker) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Location Information");
+        builder.setMessage("Have you found this location");
+
+        builder.setPositiveButton("Found Location", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (marker != null) {
+
+                    marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                }
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
