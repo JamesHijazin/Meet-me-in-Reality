75ec46e6cc10dcc8066213b9bfb387e45af09c8a
[vanessa.wijaya s3796824] <s3796824@student.rmit.edu.au>
2023-10-29 22:49:23 +0700

    fixing bugs

--- app/src/main/java/com/example/meetmereality/DiscoverActivity.java
+++ app/src/main/java/com/example/meetmereality/DiscoverActivity.java
@@ -7,20 +7,20 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class IntroductionActivity extends AppCompatActivity {
-
+public class DiscoverActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_introduction);
+        setContentView(R.layout.splash_opening_3);
 
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(IntroductionActivity.this, DiscoverActivity.class);
+                Intent intent = new Intent(DiscoverActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
     }
 }
+
